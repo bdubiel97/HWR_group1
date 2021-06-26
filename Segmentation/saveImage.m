49655df158714ddea221c65b713acdf707e5121c
Rick <dejonge00@live.nl>
2021-06-26 19:32:37 +0200

    Saving images threshold updated

--- Segmentation/saveImage.m
+++ Segmentation/saveImage.m
@@ -1,16 +1,14 @@
 function saveImage(image, name, output_size)
     
     %1. Size of the image within the defined range
-    if ~all(size(image) < output_size) 
-        ratio = max(size(image));
-        ratio = 100/ratio;
-        image = imresize(image, floor(ratio*size(image)), 'bilinear');
-    end
+    ratio = max(size(image));
+    ratio = 100/ratio;
+    image = imresize(image, floor(ratio*size(image)), 'bilinear');
        
     [image, ratio] = centerObject(image, output_size);
 
     %2. Dots-like objects treated as failure
-    if ratio < 0.035 
+    if (0.7 < ratio) || (ratio < 0.035) 
         path = fullfile(pwd, "output_failures");
     else
         path = fullfile(pwd, "output"); 
