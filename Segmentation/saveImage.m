7c7355646b60ef14ff48a812be0fd219cba679bf
Rick <dejonge00@live.nl>
2021-06-26 17:07:35 +0200

    saveImage resizing images larger than the output allows

--- Segmentation/saveImage.m
+++ Segmentation/saveImage.m
@@ -1,33 +1,22 @@
 function saveImage(image, name, output_size)
     
     %1. Size of the image within the defined range
-    if all(size(image) < output_size) 
-        [image, ratio] = centerObject(image, output_size);
-        
-        %2. Dots-like objects treated as failure
-        if ratio < 0.035 
-            path = fullfile(pwd, "output_failures");
-        else
-            path = fullfile(pwd, "output");
-        end
-            
-    %1. Size of the image extending the defined range  
-    else 
-        image = imresize(image, [100 100], 'bilinear');
-        [image, ratio] = centerObject(image, output_size);
-            
-        %2. Dots-like objects treated as failure
-        if ratio < 0.035 
-            path = fullfile(pwd, "output_failures");
-        else
-            path = fullfile(pwd, "output"); 
-        end
-
-        
+    if ~all(size(image) < output_size) 
+        ratio = max(size(image));
+        ratio = 100/ratio;
+        image = imresize(image, floor(ratio*size(image)), 'bilinear');
     end
+       
+    [image, ratio] = centerObject(image, output_size);
+
+    %2. Dots-like objects treated as failure
+    if ratio < 0.035 
+        path = fullfile(pwd, "output_failures");
+    else
+        path = fullfile(pwd, "output"); 
+    end
+    
     image = ~image;
     disp(fullfile(path, name));
     imwrite(image, fullfile(path, name));
-
-    
 end
