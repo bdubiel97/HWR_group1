5f5c0a8d4ab03bc6971a89fe1cdde2f609acd5f0
bdubiel97 <65966243+bdubiel97@users.noreply.github.com>
2021-06-26 16:14:48 +0200

    Update saveImage.m
    images extending defined range resized and saved in the "output" directory

--- Segmentation/saveImage.m
+++ Segmentation/saveImage.m
@@ -13,32 +13,16 @@ function saveImage(image, name, output_size)
             
     %1. Size of the image extending the defined range  
     else 
-        image = imresize(image, 0.5, 'bilinear');
-        
-        %2. Size of the image after resizing within the defined range
-        if (output_size - size(image)) > 0 
-            [image, ratio] = centerObject(image, output_size);
+        image = imresize(image, [100 100], 'bilinear');
+        [image, ratio] = centerObject(image, output_size);
             
-            %3. Dots-like objects treated as failure
-            if ratio < 0.035 
-                path = fullfile(pwd, "output_failures");
-
-            else
-                isLamed = checkLamed(image); 
-                %4. Saving "lamed", the rest excluded
-                if isLamed == 1
-                    path = fullfile(pwd, "output"); 
-                else
-                    path = fullfile(pwd, "output_failures");
-                end
-
-            end
-
-        %2. Size of the image after resizing extending the defined range
-        else 
-           path = fullfile(pwd, "output_failures"); 
-            
+        %2. Dots-like objects treated as failure
+        if ratio < 0.035 
+            path = fullfile(pwd, "output_failures");
+        else
+            path = fullfile(pwd, "output"); 
         end
+
         
     end
     image = ~image;
