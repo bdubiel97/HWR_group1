93212e419a2786bc23151c3cb27adf67841fda7c
EKCollins <e.k.collins@student.rug.nl>
2021-06-27 15:01:58 +0200

    final SOM

--- styleClassification/OtherAttempts/SOM/train_SOM.py
+++ styleClassification/OtherAttempts/SOM/train_SOM.py
@@ -1,3 +1,6 @@
+'''
+File for training the Self-organizing Map
+'''
 import numpy as np
 import imageio
 from glob import glob, iglob
@@ -9,27 +12,33 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 
+img_size = 64
+
 def load_data():
+	'''
+	This function loads the data from "characters_for_style_classification_morph" 
+	per class and creates 6 arrays, data and labels for each class
+	'''
 	archaic = []
 	hasmonean = []
 	herodian = []
-	arc = [f for f in iglob('characters_for_style_classification/Archaic/**/*.jpg', recursive=True) if os.path.isfile(f)]
-	has = [f for f in iglob('characters_for_style_classification/Hasmonean/**/*.jpg', recursive=True) if os.path.isfile(f)]
-	her = [f for f in iglob('characters_for_style_classification/Herodian/**/*.jpg', recursive=True) if os.path.isfile(f)]
+	arc = [f for f in iglob('characters_for_style_classification_arc/Archaic/**/*.jpg', recursive=True) if os.path.isfile(f)]
+	has = [f for f in iglob('characters_for_style_classification_arc/Hasmonean/**/*.jpg', recursive=True) if os.path.isfile(f)]
+	her = [f for f in iglob('characters_for_style_classification_arc/Herodian/**/*.jpg', recursive=True) if os.path.isfile(f)]
 
 	for image in arc:
 		im = imageio.imread(image)
-		im = np.resize(im, (64, 64, 3))
+		im = np.resize(im, (img_size, img_size, 3))
 		im = im.reshape(np.prod(im.shape))
 		archaic.append(im)
 	for image in has:
 		im = imageio.imread(image)
-		im = np.resize(im, (64, 64, 3))
+		im = np.resize(im, (img_size, img_size, 3))
 		im = im.reshape(np.prod(im.shape))
 		hasmonean.append(im)
 	for image in her:
 		im = imageio.imread(image)
-		im = np.resize(im, (64, 64, 3))
+		im = np.resize(im, (img_size, img_size, 3))
 		im = im.reshape(np.prod(im.shape))
 		herodian.append(im)
 
@@ -39,9 +48,11 @@ def load_data():
 	return archaic, hasmonean, herodian, arc_labels, has_labels, her_labels
 
 def classifySOM(som, data, x_data, y_data):
+	'''
+	This function classifies the testing data per character.
+	'''
 	winmap = som.labels_map(x_data, y_data)
 	default_class = np.sum(list(winmap.values())).most_common()[0][0]
-	print(default_class)
 	result = []
 
 	for d in data: 
@@ -53,37 +64,32 @@ def classifySOM(som, data, x_data, y_data):
 	return result
 
 #################################################################
+if __name__ == "__main__":
+	# load the data
+	archaic, hasmonean, herodian, arc_labels, has_labels, her_labels = load_data()
+	print(len(archaic), len(hasmonean), len(herodian))
 
-archaic, hasmonean, herodian, arc_labels, has_labels, her_labels = load_data()
-print(len(archaic), len(hasmonean), len(herodian))
-# concatinate data
-data = list(itertools.chain(archaic,hasmonean, herodian))
-labels = list(itertools.chain(arc_labels, has_labels, her_labels))
-print(len(data))
+	# split and concatinate the data
+	data = list(itertools.chain(archaic,hasmonean, herodian))
+	labels = list(itertools.chain(arc_labels, has_labels, her_labels))
+	x_train, x_test, y_train, y_test = train_test_split(data, labels, stratify=labels)
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels, stratify=labels)
-print("num archaic: ", y_train.count("Archaic"))
-print("num hasmonean: ", y_train.count("Hasmonean"))
-print("num herodian: ", y_train.count("Herodian"))
+	# create the self-organizing map
+	som = MiniSom(6, 6, len(data[0]), learning_rate=0.5, sigma=3)
+	som.train_random(x_train, 100) # train
+	som.pca_weights_init(x_train)
+	print("SOM has been trained.")
 
-som = MiniSom(25, 25, len(data[0]), learning_rate=0.5, sigma=3)
-som.train_random(x_train, 100)
-som.pca_weights_init(x_train)
-with open('som15x15.p', 'wb') as outfile:
-	pickle.dump(som, outfile)
+	with open('som.p', 'wb') as outfile:
+		pickle.dump(som, outfile)
+	print("SOM has been saved.")
 
-classifications = classifySOM(som, x_test, x_train, y_train)
-print("num archaic: ", classifications.count("Archaic"), "Y TEST: ", y_test.count("Archaic"))
-print("num hasmonean: ", classifications.count("Hasmonean"), "Y TEST: ", y_test.count("Hasmonean"))
-print("num herodian: ", classifications.count("Herodian"), "Y TEST: ", y_test.count("Herodian"))
-print(accuracy_score(y_test, classifications))
-print(classification_report(y_test, classifications))
+	print("Classifying...")
+	classifications = classifySOM(som, x_test, x_train, y_train) # classify the testing data
 
-""" 
-accuracies: 
-6x6 = 0.35700934579439253
-10x10 = 0.35677570093457944
-15x15 = 0.3733644859813084
-20x20 = 
-25x25 = 0.3616822429906542
-"""
+	# printing the classification results
+	print("num archaic: ", classifications.count("Archaic"), "Y TEST: ", y_test.count("Archaic"))
+	print("num hasmonean: ", classifications.count("Hasmonean"), "Y TEST: ", y_test.count("Hasmonean"))
+	print("num herodian: ", classifications.count("Herodian"), "Y TEST: ", y_test.count("Herodian"))
+	print(accuracy_score(y_test, classifications))
+	print(classification_report(y_test, classifications))
