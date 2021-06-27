f833ab47a2662b097a607d16fad6b4de39d057cb
EKCollins <e.k.collins@student.rug.nl>
2021-06-27 11:09:55 +0200

    start to reorganize style

--- styleClassification/styleImageGenerator.py
+++ styleClassification/styleImageGenerator.py
@@ -12,12 +12,6 @@ from imagemorph import elastic_morphing
 #Set this to the data directory with subfolders per character. The augmented data will be added into it. 
 imDir = "characters_for_style_classification/Archaic"
 amp, sigma = 0.9, 9
-#imSize = 64
-
-kernel = np.ones((3,3), np.uint8)
-M = np.float32([[1, 0.2, 0],
-             	[0, 1  , 0],
-            	[0, 0  , 1]])
 
 folders = os.listdir(imDir)
 if '.DS_Store' in folders:
@@ -25,48 +19,13 @@ if '.DS_Store' in folders:
 
 for dirExtension in folders:
 	path = imDir + "/" + dirExtension + "/"
-	print(path)
 	for file in os.listdir(path):
 		im = cv.imread(path + file)
 
 		h, w, _ = im.shape
-		#Matrix 1 for shearing
-		M1 = np.float32([[1, -0.2, 0], [0, 1, 0]])
-		M1[0,2] = -M1[0,1] * w/2
-		M1[1,2] = -M1[1,0] * h/2
-		#Matrix 2 for shearing
-		M2 = np.float32([[1, 0, 0], [0.2, 1, 0]])
-		M2[0,2] = -M2[0,1] * w/2
-		M2[1,2] = -M2[1,0] * h/2
 
-
-		#Create all new augmented versions of im
 		#This if-statement is required to avoid messed up images and warnings
 		if w > 27: 
 			imMorph = elastic_morphing(im, amp, sigma, h, w)
-		imErode = cv.erode(im, kernel)
-		imDilate = cv.dilate(im, kernel)
-		imShearedRight = cv.warpAffine(im, M1, (w, h), borderValue=(255,255,255))
-		imShearedLeft = cv.warpAffine(im, M2, (w, h), borderValue=(255,255,255))
-		
-
-
-		  ## Use these to see the effect of erosion and dilation imediately
-		#cv.imshow('Erosion', imErode)
-		#cv.imshow('dilation', imDilate)
-		#cv.waitKey(0)
-		
-		#Write all images to the folder
-		if w > 25:
+			#Write all images to the folder
 			cv.imwrite(path + file + "Morph.jpg", imMorph)
-		cv.imwrite(path + file + "Erode.jpg", imErode)
-		cv.imwrite(path + file + "Dilate.jpg", imDilate)
-		cv.imwrite(path + file + "RightShear.jpg", imShearedRight)
-		cv.imwrite(path + file + "LefttShear.jpg", imShearedLeft)
-
-
-
-
-
-
-
